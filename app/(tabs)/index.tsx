96e0d6bc4c411464cd85f13e510922a61d4e7ab6
Michael Tan <michael.chinyuan@gmail.com>
2025-10-06 22:49:31 -0400

    10/6/25

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,27 @@
-import { Text } from 'react-native';
+import Post, { PostType } from '@/components/post';
+import { ThemedView } from '@/components/themed-view';
+import { StyleSheet } from 'react-native';
 
 export default function Feed() {
+  const myPost: PostType = {
+    id: '1',
+    user: {
+      username: 'Mikita',
+    },
+    text: "Spain has a GDP per capita comparable to Japan and yet the Spaniards basically don't work",
+    timestamp: '2024-06-01T12:00:00Z',
+    likes: 10,
+  }
+
   return(
-    <Text> Hello </Text>
+    <ThemedView style={styles.container}>
+      <Post post={myPost} />
+    </ThemedView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 50,
+  },
+});
--- components/post.tsx
+++ components/post.tsx
@@ -1,11 +1,62 @@
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
+import React from 'react';
+import { Image, StyleSheet } from 'react-native';
 
-export default function Post({ post }) {
+export type PostType = {
+  id: string;
+  user: {
+    username: string;
+    avatar?: string;
+  }
+  text: string;
+  timestamp: string;
+  likes: number;
+}
+
+type Props = {
+  post: PostType;
+}
+
+export default function Post({ post }: Props) {
   return (
-    <ThemedView>
-      <ThemedText>{post.user}</ThemedText>
-      <ThemedText>{post.content}</ThemedText>
+    <ThemedView style={styles.container}>
+      <Image 
+        source={post.user.avatar ? { uri: post.user.avatar } : require('@/assets/images/Default_pfp.jpg')} 
+        style={styles.avatar} 
+      />
+
+      <ThemedView style={styles.content}>
+        <ThemedText type="defaultSemiBold">{post.user.username}</ThemedText>
+        <ThemedText style={styles.posttext}>{post.text}</ThemedText>
+
+        <ThemedView style={styles.reactions}>
+
+        </ThemedView>
+      </ThemedView>
     </ThemedView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    gap: 12,
+    flexDirection: 'row',
+  },
+  avatar: {
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 20,
+  },
+  content: {
+    flex: 1,
+  },
+  posttext: {
+    flexShrink: 1,
+    flexWrap: 'wrap',
+  },
+  reactions: {
+    flexDirection: 'row',
+  },
+});
